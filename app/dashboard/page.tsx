3f5f3caa2fe231b6ad602e41175a88b81b2f3f96
Zohaib Haider <narejozohaib33@gmail.com>
2026-06-01 08:42:05 +0500

    Redesign for FitTree branding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { redirect } from "next/navigation";
+// rm - rf.nextimport { redirect } from "next/navigation";
 
 export default function DashboardIndex() {
   redirect("/dashboard/plans");
